f6059b4fdb1286e2e4687fca81d13625f12e03f2
LntAxe <ertusari0@gmail.com>
2024-09-13 23:24:16 +0300

    feat: update login method to return ResponseLoginDTO and include DTO conversion

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import { ResponseLoginDTO } from '@auth/dtos/response-login.dto';
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthLoginDTO } from '../dtos/login.dto';
 import { AuthService } from '../services/auth.service';
@@ -7,7 +8,8 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('/login')
-    async login(@Body() loginDto: AuthLoginDTO) {
-        return await this.authService.login(loginDto);
+    async login(@Body() loginDto: AuthLoginDTO): Promise<ResponseLoginDTO> {
+        const token = await this.authService.login(loginDto);
+        return ResponseLoginDTO.toDTO(token);
     }
 }
