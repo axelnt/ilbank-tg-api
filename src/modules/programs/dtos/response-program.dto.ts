d348df6537a368f31ba6ef9abafc87f59a248216
LntAxe <ertusari0@gmail.com>
2024-09-13 22:43:34 +0300

    feat: enhance ResponseProgramDTO with class-transformer for serialization

--- src/modules/programs/dtos/response-program.dto.ts
+++ src/modules/programs/dtos/response-program.dto.ts
@@ -1,20 +1,29 @@
 import { Department } from '@common/entities/department.entity';
 import { Program } from '@common/entities/program.entity';
+import { Expose, plainToInstance } from 'class-transformer';
 
 export class ResponseProgramDTO {
+    @Expose()
     code: string;
+
+    @Expose()
     name: string;
+
+    @Expose()
     users: string[];
+
+    @Expose()
     departments: Department[];
+
+    @Expose()
     directorates: Department[];
+
+    @Expose()
     processBased: boolean;
 
-    constructor(program: Program) {
-        this.code = program.code;
-        this.name = program.name;
-        this.users = program.users;
-        this.departments = program.departments;
-        this.directorates = program.directorates;
-        this.processBased = program.processBased;
+    static toDTO<T extends Program | Program[]>(
+        program: T,
+    ): T extends Program[] ? ResponseProgramDTO[] : ResponseProgramDTO {
+        return plainToInstance(ResponseProgramDTO, program) as any;
     }
 }
